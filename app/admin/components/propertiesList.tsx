1855cb9cd7f1abfa181d7a359d1e65f69aedab32
Muhammad Hamid <61172566+hamidkhans651@users.noreply.github.com>
2025-01-04 15:59:20 +0500

    udpate.

--- app/admin/components/propertiesList.tsx
+++ app/admin/components/propertiesList.tsx
@@ -1,10 +1,23 @@
 "use client";
 
+import {
+    Navbar,
+    NavbarBrand,
+    NavbarContent,
+    NavbarItem,
+    Link,
+    DropdownItem,
+    DropdownTrigger,
+    Dropdown,
+    DropdownMenu,
+    Avatar,
+} from "@nextui-org/react";
+
 import { useState, useEffect } from "react";
 import { Input, Pagination, PaginationItemRenderProps, PaginationItemType, CircularProgress } from "@nextui-org/react";
 import { Card, CardBody, CardFooter, Image, Button } from "@nextui-org/react";
 import { Loader } from "lucide-react";
-import { Ellipsis } from "lucide-react"
+import { Ellipsis, Share2 } from "lucide-react"
 import { ChevronIcon } from "@/app/Properties/icons/ChevronIcon";
 import { Property } from "@/types/property"; // Use the Property type
 
@@ -102,13 +115,35 @@ export default function PropertiesList() {
                                     className="w-full object-cover h-100%"
                                     src={property.imageUrl} // Dynamic image URL
                                 />
+                        
+                                <Dropdown >
+                                    <DropdownTrigger>
+                                        <Avatar
+                                            // isBordered
+                                            as="button"
+                                            className="transition-transform absolute top-2 right-2 z-10"
+                                            // color="defual"
+                                            name="button"
+                                            size="sm"
+                                            src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLWVsbGlwc2lzIj48Y2lyY2xlIGN4PSIxMiIgY3k9IjEyIiByPSIxIi8+PGNpcmNsZSBjeD0iMTkiIGN5PSIxMiIgcj0iMSIvPjxjaXJjbGUgY3g9IjUiIGN5PSIxMiIgcj0iMSIvPjwvc3ZnPg=="
+                                        />
+                                    </DropdownTrigger>
+                                    <DropdownMenu aria-label="Profile Actions" variant="flat">
+                                 
+                                        <DropdownItem key="Delete">Delete</DropdownItem>
+                                        <DropdownItem key="team_settings">Update</DropdownItem>
+                                    </DropdownMenu>
+                                </Dropdown>
+
                                 <Button
+
                                     size="sm"
                                     isIconOnly
                                     // color="none"
                                     aria-label="Like"
-                                    className="absolute top-2 right-2 z-10">
-                                    <Ellipsis />
+                                    className="absolute top-2 right-12 z-10">
+                                    <Share2 />
+
                                 </Button>
                                 <CardBody className="overflow-visible p-3">
                                     <p className="text-xl text-start">{property.price}</p>
