578b6ce05bad1163ba7c61666e60dda8d93cb83a
Muhammad Hamid <61172566+hamidkhans651@users.noreply.github.com>
2025-01-24 15:20:59 +0500

    update.schema

--- server/schema.ts
+++ server/schema.ts
@@ -28,31 +28,6 @@ export const users = pgTable("user", {
   skillLevel: SkillLevelEnum("skill_level").notNull().default("Buyer"),
 })
 
-// export const properties = pgTable("properties", {
-//   id: serial("id").primaryKey(),
-//   title: varchar("title", { length: 255 }).notNull(),
-//   description: text("description").notNull(),
-//   price: integer("price").notNull(),
-//   location: varchar("location", { length: 255 }).notNull(),
-//   imageUrl: varchar("image_url", { length: 500 }).notNull(),
-//   bedrooms: integer("bedrooms").notNull(),
-//   bathrooms: integer("bathrooms").notNull(),
-//   sqft: integer("sqft").notNull(),
-//   propertyType: varchar("property_type", { length: 255 }).notNull(),
-//   isForSale: boolean("is_for_sale").notNull().default(true), // New column
-// });
-
-
-
-// export const images = pgTable("images", {
-//   id: uuid("id").defaultRandom().primaryKey(),         // Unique identifier
-//   fileName: varchar("file_name", { length: 255 }).notNull(), // Original file name
-//   fileUrl: text("file_url").notNull(),                // Image URL from ImageKit
-//   description: text("description"),                   // Optional description
-//   createdAt: timestamp("created_at", { withTimezone: true }).defaultNow().notNull(), // Upload timestamp
-// });
-
-
 export const properties = pgTable("properties", {
   id: serial("id").primaryKey(),
   zpid: varchar("zpid", { length: 50 }).unique().notNull(), // Zillow Property ID
