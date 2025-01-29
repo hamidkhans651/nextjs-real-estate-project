6af58c3bbd28d8e4b2ff97dfa2d01b716d26ef62
Muhammad Hamid <61172566+hamidkhans651@users.noreply.github.com>
2025-01-29 21:04:15 +0500

    x

--- components/SaveShare.tsx
+++ components/SaveShare.tsx
@@ -6,14 +6,14 @@ const SaveShare = () => {
     <div className="flex items-center space-x-6">
       {/* Save Section */}
       <div className="flex items-center">
-        <Heart className="w-6 h-6 text-gray-700" />
-        <span className="hidden sm:inline-block text-gray-700 ml-2">Save</span>
+        <Heart className="w-6 h-6 " />
+        <span className="hidden sm:inline-block  ml-2">Save</span>
       </div>
 
       {/* Share Section */}
       <div className="flex items-center">
-        <Share className="w-6 h-6 text-gray-700" />
-        <span className="hidden sm:inline-block text-gray-700 ml-2">Share</span>
+        <Share className="w-6 h-6 " />
+        <span className="hidden sm:inline-block  ml-2">Share</span>
       </div>
     </div>
   );
